da55de0eea7a9ca576c48e76a3599bdee75de9f7
S.Saha <soumabhasaha1509+github@gmail.com>
2025-10-06 00:42:38 +0530

    some config changed

--- client/src/utility/axios-base.ts
+++ client/src/utility/axios-base.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const base = axios.create({
-  baseURL: import.meta.env.VITE_CORS_URL,
+  baseURL: '/api',
   withCredentials: true,
   validateStatus: (_) => true
 });
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,22 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  const targetUrl = env.VITE_CORS_URL;
+  return {
+    plugins: [react(), tailwindcss(), flowbiteReact()],
+    server: {
+      proxy: {
+        '/api': {
+          target: targetUrl,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
+      },
     },
-  },
+  };
 });
